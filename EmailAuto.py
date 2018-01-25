84b35ea5883587791cec320e4e4312c81ef9e8ae
MaxWayne <maximilienpelletier@gmail.com>
2018-01-25 12:28:54 +0100

    Update EmailAuto.py

--- EmailAuto.py
+++ EmailAuto.py
@@ -1,1 +1,45 @@
-import pandas as pd
+fromaddr = "spartacus.automation@gmail.com"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "Status2013")
+
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+def py_mail(SUBJECT, BODY, TO, FROM):
+    """With this function we send out our html email"""
+ 
+    # Create message container - the correct MIME type is multipart/alternative here!
+    MESSAGE = MIMEMultipart('alternative')
+    MESSAGE['subject'] = SUBJECT
+    MESSAGE['To'] = ", ".join(TO)
+    MESSAGE['From'] = FROM
+    MESSAGE.preamble = """
+Your mail reader does not support the report format.
+Please visit us <a href="http://www.mysite.com">online</a>!"""
+ 
+    # Record the MIME type text/html.
+    HTML_BODY = MIMEText(BODY, 'html')
+ 
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    MESSAGE.attach(HTML_BODY)
+ 
+    # The actual sending of the e-mail
+    server = smtplib.SMTP('smtp.gmail.com:587')
+ 
+    # Print debugging output when testing
+    if __name__ == "__main__":
+        server.set_debuglevel(1)
+ 
+    # Credentials (if needed) for sending the mail
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "Status2013")
+
+
+    server.sendmail(FROM, TO, MESSAGE.as_string())
+    server.quit()
